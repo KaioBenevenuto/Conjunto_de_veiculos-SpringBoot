a16ae386e396f8c35e2b9165295e2e131854384b
Kaio Benevenuto Hoffman <kaiohoffmanbeneve@gmail.com>
2025-09-19 22:28:10 -0300

    2-adicionei atributos em veiculo

--- src/main/java/com/conjunto_de_veiculos/veiculos/model/Veiculo.java
+++ src/main/java/com/conjunto_de_veiculos/veiculos/model/Veiculo.java
@@ -1,5 +1,27 @@
 package com.conjunto_de_veiculos.veiculos.model;
 
-public class Veiculo {
+import java.util.UUID;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.Data;
 
+@Data
+public class Veiculo {
+	
+	@Id
+	private UUID id = UUID.randomUUID();
+
+	//Não pode ter placas iguais
+	@Column(nullable = false)
+	private String placa;
+	
+	@Column(nullable = false)
+	private String marca;
+	
+	@Column(nullable = false)
+	private String modelo;
+	
+	@Column(nullable = false)
+	private int ano;
+	
 }
